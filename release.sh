1fae1c1ea4fab9859416e5647af19d6e411d31b0
David Neubauer <davidneub@gmail.com>
2017-05-08 17:28:31 +0200

    Add next semver

--- release.sh
+++ release.sh
@@ -4,7 +4,20 @@ ERROR_CATEGORY="Expected categroy <major|minor|patch> passed as first argument";
 ERROR_CURRENT="Expected current semver <X.Y.Z> passed as second argument";
 
 CATEGORY=${1:?$ERROR_CATEGORY};
-CURRENT=${1:?$ERROR_CURRENT};
+CURRENT=${2:?$ERROR_CURRENT};
+
+function next() {
+  if [ $1 == "patch" ]
+  then
+    echo "1.2.1";
+  elif [ $1 == "minor" ]
+  then
+    echo "1.3.0";
+  elif [ $1 == "major" ]
+  then
+    echo "2.2.0";
+  fi
+}
 
 if `echo $1 | egrep -q '^(major|minor|patch)$'` 
 then
@@ -13,6 +26,18 @@ else
   echo "Invalid category '$1' passed. $ERROR_CATEGORY";
   exit 1;
 fi
+
+# Check semver e.g.: 1.2.0
+# Note: 1.2.0-beta is not supported
+if `echo $2 | egrep -q '^((\d*\.){2}\d)$'`
+then
+  NEXT=`next $1 $2`;
+  echo "Preparing $NEXT";
+else
+  echo "Invalid semver '$2' passed. $ERROR_CURRENT";
+  exit 1;
+fi
+
 
 echo "Bumping to $CATEGORY";
 
--- tests.sh
+++ tests.sh
@@ -32,12 +32,23 @@ function test() {
 }
 
 ERROR_CATEGORY="Expected categroy <major|minor|patch> passed as first argument";
+ERROR_CURRENT="Expected current semver <X.Y.Z> passed as second argument";
 
 test "sh release.sh" "throws when no argument is passed" "$ERROR_CATEGORY";
-test "sh release.sh beta" "throws when invalid semver is passed" "Invalid category 'beta' passed. $ERROR_CATEGORY";
-test "sh release.sh major" "logs major semver" "Preparing major";
-test "sh release.sh minor" "logs minor semver" "Preparing minor";
-test "sh release.sh patch" "logs minor patch" "Preparing patch";
-test "sh release.sh major" "logs bumping major" "Bumping to major";
-test "sh release.sh minor" "logs bumping minor" "Bumping to minor";
-test "sh release.sh patch" "logs bumping patch" "Bumping to patch";
+test "sh release.sh beta" "throws when only one argument is passed" "$ERROR_CURRENT";
+
+test "sh release.sh beta 0.0.0" "throws when invalid category is passed" "Invalid category 'beta' passed. $ERROR_CATEGORY";
+
+test "sh release.sh major 0.0.0" "logs major semver" "Preparing major";
+test "sh release.sh minor 0.0.0" "logs minor semver" "Preparing minor";
+test "sh release.sh patch 0.0.0" "logs minor patch" "Preparing patch";
+
+test "sh release.sh major 0.0.0" "logs bumping major" "Bumping to major";
+test "sh release.sh minor 0.0.0" "logs bumping minor" "Bumping to minor";
+test "sh release.sh patch 0.0.0" "logs bumping patch" "Bumping to patch";
+
+test "sh release.sh patch 1.2.0-beta" "throws when invalid semver is passed" "Invalid semver '1.2.0-beta' passed. $ERROR_CURRENT";
+
+test "sh release.sh patch 1.2.0" "logs next semver" "Preparing 1.2.1";
+test "sh release.sh minor 1.2.0" "logs next semver" "Preparing 1.3.0";
+test "sh release.sh major 1.2.0" "logs next semver" "Preparing 2.2.0";
